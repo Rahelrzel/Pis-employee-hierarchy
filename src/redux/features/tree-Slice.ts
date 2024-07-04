fee52cd1ac6b0cbd8515f646ca974358f9802ad8
Rahelrzel <rahelrzele@gmail.com>
2024-07-04 10:14:58 +0300

    final commit

--- src/redux/features/tree-Slice.ts
+++ src/redux/features/tree-Slice.ts
@@ -1,40 +1,39 @@
-// slices/treeSlice.ts
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { TreeNode } from "@/types/tree";
+// import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+// import { TreeNode } from "@/types/tree";
 
-interface TreeState {
-  isLoading: boolean;
-  error?: string;
-  data: TreeNode[];
-}
+// interface TreeState {
+//   isLoading: boolean;
+//   error?: string;
+//   data: TreeNode[];
+// }
 
-const initialState: TreeState = {
-  isLoading: false,
-  error: undefined,
-  data: [],
-};
+// const initialState: TreeState = {
+//   isLoading: false,
+//   error: undefined,
+//   data: [],
+// };
 
-const treeSlice = createSlice({
-  name: "tree",
-  initialState,
-  reducers: {
-    loadTreeStart: (state) => {
-      state.isLoading = true;
-      state.error = undefined;
-    },
-    loadTreeSuccess: (state, action: PayloadAction<TreeNode[]>) => {
-      state.isLoading = false;
-      state.data = action.payload;
-      state.error = undefined;
-    },
-    loadTreeError: (state, action: PayloadAction<string>) => {
-      state.isLoading = false;
-      state.data = [];
-      state.error = action.payload;
-    },
-  },
-});
+// const treeSlice = createSlice({
+//   name: "tree",
+//   initialState,
+//   reducers: {
+//     loadTreeStart: (state) => {
+//       state.isLoading = true;
+//       state.error = undefined;
+//     },
+//     loadTreeSuccess: (state, action: PayloadAction<TreeNode[]>) => {
+//       state.isLoading = false;
+//       state.data = action.payload;
+//       state.error = undefined;
+//     },
+//     loadTreeError: (state, action: PayloadAction<string>) => {
+//       state.isLoading = false;
+//       state.data = [];
+//       state.error = action.payload;
+//     },
+//   },
+// });
 
-export const { loadTreeStart, loadTreeSuccess, loadTreeError } =
-  treeSlice.actions;
-export default treeSlice.reducer;
+// export const { loadTreeStart, loadTreeSuccess, loadTreeError } =
+//   treeSlice.actions;
+// export default treeSlice.reducer;
